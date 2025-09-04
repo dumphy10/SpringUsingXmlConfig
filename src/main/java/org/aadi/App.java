c8cf1de5cfcb9247eb2c83f074bec40072a9864d
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-04 22:01:18 +0530

    Creating spring.xml file in resources folder. adding xml dependencies from spring 6 bean configuration xml

--- src/main/java/org/aadi/App.java
+++ src/main/java/org/aadi/App.java
@@ -1,5 +1,8 @@
 package org.aadi;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,8 +11,9 @@ public class App
 {
     public static void main( String[] args )
     {
-
-     Alien obj = new Alien();
+    ApplicationContext context;
+        context = new ClassPathXmlApplicationContext("spring.xml");
+        Alien obj = (Alien) context.getBean("alien");
      obj.code();
     }
 }
