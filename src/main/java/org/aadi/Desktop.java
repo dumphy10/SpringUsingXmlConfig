0bbc0ed54c7a04c707c1a76a6c661a048bbcf9a5
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-05 22:07:31 +0530

    Created a Desktop class

---
+++ src/main/java/org/aadi/Desktop.java
@@ -0,0 +1,12 @@
+package org.aadi;
+
+public class Desktop {
+
+    public Desktop(){
+        System.out.println("Desktop called");
+    }
+
+    public void compile(){
+        System.out.println("Compiling code in desktop");
+    }
+}
